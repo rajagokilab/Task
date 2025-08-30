0cbaaf73b521fec62b182b34bef6c1c3de4d1489
Rajagokila B <rajagokilab@gmail.com>
2025-08-30 11:33:22 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
+// App.jsx
 import React from "react";
-import StudentList from "./components/StudentList";
- 
+import ProductList from "./components/ProductList";
+
 const App = () => {
+  const products = [
+    {
+      title: "iPhone 15",
+      price: 79999,
+      isAvailable: true,
+      discount: 10,
+      image:
+        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone15",
+    },
+    {
+      title: "Samsung Galaxy S23",
+      price: 74999,
+      isAvailable: false,
+      image:
+        "https://images.samsung.com/is/image/samsung/p6pim/galaxy-s23",
+    },
+    {
+      title: "OnePlus 11",
+      price: 59999,
+      isAvailable: true,
+      discount: 5,
+      image:
+        "https://image01.oneplus.net/ebp/2023/oneplus11.png",
+    },
+  ];
+
   return (
-    <div style={styles.app}>
-      <h1>🎓 Student Profile Dashboard</h1>
-      <StudentList />
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6 text-center">
+        🛒 Product Listings
+      </h1>
+      <ProductList products={products} />
     </div>
   );
 };
- 
-const styles = {
-  app: {
-    textAlign: "center",
-    fontFamily: "Arial, sans-serif",
-    margin:"80px",
-    padding: "100px",
-    backgroundColor: "#f5f5f5",
-    minHeight: "100vh",
-  },
-};
- 
+
 export default App;
